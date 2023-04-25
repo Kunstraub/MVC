4895e85c42add42644ff4441590306e6d28b5800
Stefan Buttchereit <s.buttchereit@mail.de>
2023-04-25 20:55:28 +0200

    add CorsConfiguration bug fixed

--- src/main/java/com/exercise/MVC/MvcApplication.java
+++ src/main/java/com/exercise/MVC/MvcApplication.java
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
+@EnableWebMvc
 public class MvcApplication {
 
 	public interface SaySomethingService{
@@ -119,21 +121,8 @@ public class MvcApplication {
 		 */
 	}
 
-	@Bean
-	public CorsFilter corsFilter(){
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedOrigins(Arrays.asList("Origin", "Access-Control-Allow-Origin","Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
-				"Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization",
-				"Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+
+
 
 
 
