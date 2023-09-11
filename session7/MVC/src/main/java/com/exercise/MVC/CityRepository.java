edd8295d92784cc5845e6794381346d9d9916eab
Kunstraub <49714190+Kunstraub@users.noreply.github.com>
2023-09-11 12:13:18 +0200

    Delete session7/MVC/src/main directory

--- session7/MVC/src/main/java/com/exercise/MVC/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.MVC;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends CrudRepository<City,Long> {
-    public City findByName(String name);
-}
